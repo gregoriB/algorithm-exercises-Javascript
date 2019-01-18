a4f35399b381d58e8477356b0917e42686661793
gregoriB <brandon.gregori@gmail.com>
2019-01-18 13:40:02 -0800

    small refactor

--- codewars/kata/JadenCasingStrings.js
+++ codewars/kata/JadenCasingStrings.js
@@ -1,10 +1,10 @@
 String.prototype.toJadenCase = function () {
-  let isSpace;
+  let isSpace = true;
   let newStr = "";
   for (let char of this) {
      newStr = isSpace ? newStr + char.toUpperCase() : newStr + char;
      isSpace = char === " " ? true : false;
   }
   
-  return newStr.slice(0, 1).toUpperCase() + newStr.slice(1);
+  return newStr;
 };
